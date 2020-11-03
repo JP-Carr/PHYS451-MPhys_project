3ababebbb0aa54f3f67b0506498036de804c8b23
JP-Carr <60516230+JP-Carr@users.noreply.github.com>
2020-11-03 13:03:13 +0000

    het function now runs in simulation
    - Created function to produce heterodyned data with variable parameters
    - generate_het function runs in the "simulation function" for SBI

--- HeterodynedData.py
+++ HeterodynedData.py
@@ -5,44 +5,88 @@ from matplotlib import pyplot as plt
 import sys
 import pickle as pkl
 
-parcontent = """\
-PSRJ     J0123+3456
-RAJ      01:23:45.6789
-DECJ     34:56:54.321
-F0       567.89
-F1       -1.2e-12
-PEPOCH   56789
-H0       5.12e-25
-COSIOTA  0.3
-PSI      1.1
-PHI0     2.4
-"""
+run=False
 
-# add content to the par file
-parfile = "J0123+3456.par"
-with open(parfile, "w") as fp:
-    fp.write(parcontent)
+def generate_het(H0=5.12e-25,COSIOTA=0.3,PSI=1.1,PHI0=2.4):
     
-# create some fake heterodyned data
-detector = "H1"  # the detector to use
-#times = np.linspace(1000000000.0, 1000086340.0, 1440)  # times
-times = np.linspace(1000000000.0, 1000086340.0, 1440)
-het = HeterodynedData(
-    times=times,
-    inject=True,
-    par=parfile,
-    injpar=parfile,
-    fakeasd=1e-24,
-    detector=detector,
-)
+    parname="J0123+3456"
+    
+    parcontent = """\
+    PSRJ     {}
+    RAJ      01:23:45.6789
+    DECJ     34:56:54.321
+    F0       567.89
+    F1       -1.2e-12
+    PEPOCH   56789
+    H0       {}
+    COSIOTA  {}
+    PSI      {}
+    PHI0     {}
+    """.format(parname,H0,COSIOTA,PSI,PHI0)
+    
+    parfile = parname+".par"
+    with open(parfile, "w") as fp:
+        fp.write(parcontent)
+        
+    detector = "H1"  
 
-#print(type(het.par))
-#np.save("het.npy",(het.data))
-het.write("data.txt")
+    times = np.linspace(1000000000.0, 1000086340.0, 1440)
+    het = HeterodynedData(
+        times=times,
+        inject=True,
+        par=parfile,
+        injpar=parfile,
+        fakeasd=1e-24,
+        detector=detector,
+    )
+    
+    return het
 
-prior = {}
-prior["h0"] = Uniform(0.0, 1e-22, "h0")
-prior["phi0"] = Uniform(0.0, np.pi, "phi0")
-priordic = PriorDict(prior)
-print(type(priordic))
-pkl.dump("")
+x=generate_het()
+
+print(x.data)
+
+if __name__=="__main__" and run==True:
+    
+    parcontent = """\
+    PSRJ     J0123+3456
+    RAJ      01:23:45.6789
+    DECJ     34:56:54.321
+    F0       567.89
+    F1       -1.2e-12
+    PEPOCH   56789
+    H0       5.12e-25
+    COSIOTA  0.3
+    PSI      1.1
+    PHI0     2.4
+    """
+    
+    # add content to the par file
+    parfile = "J0123+3456.par"
+    with open(parfile, "w") as fp:
+        fp.write(parcontent)
+        
+    # create some fake heterodyned data
+    detector = "H1"  # the detector to use
+    #times = np.linspace(1000000000.0, 1000086340.0, 1440)  # times
+    times = np.linspace(1000000000.0, 1000086340.0, 1440)
+    het = HeterodynedData(
+        times=times,
+        inject=True,
+        par=parfile,
+        injpar=parfile,
+        fakeasd=1e-24,
+        detector=detector,
+    )
+    
+    #print(type(het.par))
+    #np.save("het.npy",(het.data))
+    het.write("data.txt")
+    
+    prior = {}
+    prior["h0"] = Uniform(0.0, 1e-22, "h0")
+    prior["phi0"] = Uniform(0.0, np.pi, "phi0")
+    priordic = PriorDict(prior)
+    print(type(priordic))
+   # pkl.dump("")
+    
--- SBI_inference.py
+++ SBI_inference.py
@@ -3,15 +3,19 @@ import sbi.utils as utils
 from sbi.inference.base import infer
 import numpy as np
 import sys
-
+import time
 from bilby.core.prior import PriorDict, Uniform
 from PriorDictMod import PriorDictMod, distribution_array
 from torch.distributions.uniform import Uniform as torch_uni
-
+from HeterodynedData import generate_het
 import sbi.utils as utils
 
-h0=torch_uni(0.0, 1e-22)
-phi0=torch_uni(0.0, np.pi)
+start=time.time()
+sim_timer=[]
+#torch.set_default_tensor_type('torch.cuda.FloatTensor')
+
+
+#sys.exit()
 
 dist_vals={"h0": [0.0, 1e-22],
            "phi0": [0.0, np.pi]
@@ -21,16 +25,21 @@ dist_lows=torch.tensor([float(dist_vals[i][0]) for i in dist_vals])
 dist_highs=torch.tensor([float(dist_vals[i][1]) for i in dist_vals])
 
 prior = utils.BoxUniform(low=dist_lows, high=dist_highs)
-print(prior)
+print(prior.sample())
 
 
 
 def simulator(parameter_set):   #links parameters to simulation data
-    print(parameter_set.shape)
+    global sim_timer
+    start=time.time()
+    H0=float(parameter_set[0])
+    phi0=float(parameter_set[1])
+    
+    het=generate_het(H0=H0,PHI0=phi0)
+    sim_timer.append(time.time()-start)
+    return torch.from_numpy(het.data)#parameter_set
 
-    return parameter_set
-
-posterior = infer(simulator, prior, method='SNPE', num_simulations=100)
+posterior = infer(simulator, prior, method='SNPE', num_simulations=4)
 
 """
 observation = torch.zeros(3)
@@ -38,5 +47,7 @@ samples = posterior.sample((10000,), x=observation)
 log_probability = posterior.log_prob(samples, x=observation)
 _ = utils.pairplot(samples, limits=[[-2,2],[-2,2],[-2,2]], fig_size=(6,6))
 
-print("\a")
+
 """
+print("\nTime elapsed = {}s\a".format(round(time.time()-start,2)))
+print("Total Simulation Time = {}s".format(round(sum(sim_timer),2)))
