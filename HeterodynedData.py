637ae62fbcab998d414ed9f181bb72488554be5b
JP-Carr <60516230+JP-Carr@users.noreply.github.com>
2020-11-07 17:00:57 +0000

    Creation of .par files no longer required
    Introduced "PulsarParametersPy" as an alternative to the writing & reading of .par files

--- HeterodynedData.py
+++ HeterodynedData.py
@@ -4,47 +4,69 @@ from cwinpy import HeterodynedData, TargetedPulsarLikelihood
 from matplotlib import pyplot as plt
 import sys
 import pickle as pkl
+from lalpulsar.PulsarParametersWrapper import PulsarParametersPy
 
 run=False
 
 def generate_het(H0=5.12e-25,COSIOTA=0.3,PSI=1.1,PHI0=2.4):
     
+    use_parfile=False
+    
     parname="J0123+3456"
     
-    parcontent = """\
-    PSRJ     {}
-    RAJ      01:23:45.6789
-    DECJ     34:56:54.321
-    F0       567.89
-    F1       -1.2e-12
-    PEPOCH   56789
-    H0       {}
-    COSIOTA  {}
-    PSI      {}
-    PHI0     {}
-    """.format(parname,H0,COSIOTA,PSI,PHI0)
     
-    parfile = parname+".par"
-    with open(parfile, "w") as fp:
-        fp.write(parcontent)
+    if use_parfile==True:
+    
+        parcontent = """\
+        PSRJ     {}
+        RAJ      01:23:45.6789
+        DECJ     34:56:54.321
+        F0       567.89
+        F1       -1.2e-12
+        PEPOCH   56789
+        H0       {}
+        COSIOTA  {}
+        PSI      {}
+        PHI0     {}
+        """.format(parname,H0,COSIOTA,PSI,PHI0)
         
+        par = parname+".par"
+        with open(par, "w") as fp:
+            fp.write(parcontent)
+    else:
+    
+      #  print(0)
+        par = PulsarParametersPy()  # create an empty object
+        par["PSRJ"] = parname  # give it a name
+        par["RAJ"] = 1.8  # give it a right ascension (in rads)
+        par["DECJ"] = 0.54  # give it a declination (in rads)
+        par["F"] = [567.89] # give it a frequency (in Hz)
+        par["H0"] = H0  # give it a gravitational-wave amplitude
+        par["COSIOTA"] = COSIOTA  # give it a value of cos(iota) (between -1 and 1)
+        par["PSI"] = PSI  # give it a value of the polarisation angle (in rads)
+        par["PHI0"] = PHI0  # give it a value of the initial phase (in rads)
+     #   parcontent["PEPOCH"] = 1000000000
+       # print(1)
+       # print(parcontent)
+    
     detector = "H1"  
-
+   # print(2)
     times = np.linspace(1000000000.0, 1000086340.0, 1440)
     het = HeterodynedData(
         times=times,
         inject=True,
-        par=parfile,
-        injpar=parfile,
+        par=par,
+        injpar=par,
         fakeasd=1e-24,
         detector=detector,
+        bbminlength=1e10000
     )
-    
+  #  print(3)
     return het
 
-x=generate_het()
-
-print(x.data)
+if __name__=="__main__":
+    x=generate_het()
+    print(x.data)
 
 if __name__=="__main__" and run==True:
     
@@ -81,12 +103,12 @@ if __name__=="__main__" and run==True:
     
     #print(type(het.par))
     #np.save("het.npy",(het.data))
-    het.write("data.txt")
-    
+ #   het.write("data.txt")
+    """
     prior = {}
     prior["h0"] = Uniform(0.0, 1e-22, "h0")
     prior["phi0"] = Uniform(0.0, np.pi, "phi0")
     priordic = PriorDict(prior)
     print(type(priordic))
    # pkl.dump("")
-    
+    """
